97f435c3c3284530eb2942c03739154f7930e672
Luis Solís <luisolcajusiju@gmail.com>
2022-12-19 17:09:57 -0500

    Its done the first commit

---
+++ src/main/java/com/losm/spendApp/SpendAppApplication.java
@@ -0,0 +1,13 @@
+package com.losm.spendApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpendAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpendAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/losm/spendApp/SpendAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.losm.spendApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpendAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
